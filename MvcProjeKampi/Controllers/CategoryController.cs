a51540d8c3dfe20789b8a145387cda3d0ab58abf
barış Fırat arslan <barisfiratarslan@hotmail.com>
2021-04-27 16:37:01 +0300

    Updated 27.04.2021

--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,25 @@ namespace MvcProjeKampi.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager categoryManager = new CategoryManager();
+
         // GET: Category
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = categoryManager.GetAllBL();
+            return View(categoryValues);
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            categoryManager.CategoryAddBL(category);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
