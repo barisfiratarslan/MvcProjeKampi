ae9adb60d91b3540c89d56d22e92af52d4c29842
barış Fırat arslan <barisfiratarslan@hotmail.com>
2021-05-29 17:17:03 +0300

    Updated 28.05.2021

--- BusinessLayer/Abstract/IAboutService.cs
+++ BusinessLayer/Abstract/IAboutService.cs
@@ -10,9 +10,9 @@ namespace BusinessLayer.Abstract
     public interface IAboutService
     {
         List<About> GetList();
-        void CategoryAdd(About about);
+        void AboutAdd(About about);
         About GetByID(int id);
-        void CategoryDelete(About about);
-        void CategoryUpdate(About about);
+        void AboutDelete(About about);
+        void AboutUpdate(About about);
     }
 }
